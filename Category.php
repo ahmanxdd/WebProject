d477be8e68b238bf46e08c878bc650cceb19168e
ftyxyz <ftyxyz@gmail.com>
2015-07-15 05:40:44 +0800

    Category.php

--- Category.php
+++ Category.php
@@ -9,26 +9,90 @@
 	
 	//Categories
 	function getCategory($catNo) {
-		
+		$query = 'SELECT * FROM Cateory WHERE ' . catNo . " = $catNo";
+		return DB::query($query);
 	}
 	
-	function getSubCategories($catNo, $includeSelf) {
+	function getSubCategories($catNo, $includeSelf, $catNameOrder = "ASC") {
 		//use recursive
-		//desc, asc
+		$l = 0;
+		$retArr = array();
+		if ($includeSelf == true) {
+			$query = 'SELECT * FROM Category '
+			. 'WHERE ' . catNo . " = '$catNo'";
+			$retArr[$l++] = DB::query($query, true);
+		}
+		
+		$query = 'SELECT * FROM Category '
+			. 'WHERE ' . catParent . " = $catNo "
+			. 'ORDER BY ' . catName . ' '. $catNameOrder;
+		$result = DB::query($query, false);
+		if ($result === null)
+			return $retArr;
+		while($row = $result->fetch_assoc()) {
+			$subcatArr = getSubCategories($row[catNo], true, catNameOrder);
+			foreach ($subcatArr as $val)
+				$retArr[$l++] = $val;
+		}
+		return $retArr;
+	}
+	
+	function getSubCategoriesNested($catNo, $includeSelf, $catNameOrder) {
+		$l = 0;
+		$retArr = array();
+		if ($includeSelf == true) {
+			$query = 'SELECT * FROM Category '
+			. 'WHERE ' . catNo . " = '$catNo'";
+			$retArr[$l++] = DB::query($query, true);
+		}
+		
+		$query = 'SELECT * FROM Category '
+		    . 'WHERE ' . catParent . " = $catNo "
+			. 'ORDER BY ' . catName . ' '. $catNameOrder;
+		$result = DB::query($query, false);
+		if ($result === null)
+			return $retArr;
+		while($row = $result->fetch_assoc())
+			$retArr[$l++] = getSubCategories($row[catNo], true, $catNameOrder);
+		return $retArr;
+			
 	}
 	
 	function getAllCategories() {
 		//desc, asc
+		$query = 'SELECT * FROM Categories '
+			. DB::genOrderByStr(func_get_args(), func_num_args, 0);
+		return DB::query($query);
 	}
 	
 	function addCategory($catName, $catParent) {
 		if (isset($catName) && is_array($catName)) {
 			//for JSON Object
+			$catObj = $catName;
+			$catName = $catObj->catName;
+			$catParent = $catObj->catParent;
 		}
+		$catNo = DB::getLastIndex("Category");
+		
+		$query = DB::genInsertStr("Category", $catNo, $catName, $catParent);
+		return DB::query($query);
 	}
 	
 	function delCategory($catNo) {
+		$query = 'SELECT * FROM Category '
+				. 'WHERE ' . catParent . " = $catNo";
+		$result = DB::query($query, false);
+		if ($result == null) {
+			$query = 'DELETE FROM Category WHERE ' . catNo . " = $catNo";
+			return DB::query($query);
+		}
+		
 		//del all subcat first
+		while ($row = $result->fetch_assoc()) {
+			if (delCategory($row[catNo]) == false)
+				return false;
+		}
+		
 	}
 		
 ?>
