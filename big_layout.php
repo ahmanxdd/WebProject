7f35eb4a3b8354ef6e67a1c8ad36a5da5bfe50fe
ftyxyz <ftyxyz@gmail.com>
2015-07-18 20:57:23 +0800

    navbar

---
+++ big_layout.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="navbar/navbar.css" />
+	<link rel="stylesheet" type="text/css" href="css/col.css" />
+	<link rel="stylesheet" type="text/css" href="css/col-m.css" />
+	<link rel="stylesheet" type="text/css" href="css/general.css" />
+</head>
+<body>
+	<div class="navBar_C">
+		<div class="navBar inContent row">
+			<div class="navLogo col-50 col-m-0">Logo</div>
+			<div class="navTab col-8 col-m-15">Products</div>
+			<div class="navTab col-8 col-m-15">Admin</div>
+			<div class="navTab col-8 col-m-15">Schedule</div>
+			<div class="navTab col-8 col-m-15">Orders</div>
+			<div class="navTab col-8 col-m-15">SC</div>
+			<div class="navTab col-6 col-m-15">SC</div>
+			
+		</div>
+	</div>
+	
+	<div class="content_C">
+		abc
+	</div>
+</body>
+</html>
